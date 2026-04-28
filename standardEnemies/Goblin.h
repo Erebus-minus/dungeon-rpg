36dd2b8c5e074fd5ab2573c08b257f766c53117e
Diego Garcia <25twister0120@gmail.com>
2026-04-27 21:42:31 -0700

    Implemented the Goblin Enemy Class

--- standardEnemies/Goblin.h
+++ standardEnemies/Goblin.h
@@ -4,9 +4,17 @@
 
 #ifndef DUNGEON_RPG_GOBLIN_H
 #define DUNGEON_RPG_GOBLIN_H
+#include "StandardEnemy.h"
 
 
-class Goblin {
+class Goblin: public StandardEnemy {
+
+private:
+    int chanceToRun = 0.1; //Goblins are generally pathetic creatures
+                           //Whenever they are hit, they have a chance of simply fleeing.
+                           //Rn, the code will just kill them, but if you guys wanna change it feel free to
+public:
+    void changeHP(int value) override;
 };
 
 
