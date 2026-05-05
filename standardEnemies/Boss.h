fac44bd43659facea8be86010988d1b728ea79e2
TurQski <akhalil9443@gmail.com>
2026-05-04 22:12:35 -0700

    add two-phase boss enemy

---
+++ standardEnemies/Boss.h
@@ -0,0 +1,23 @@
+#ifndef DUNGEON_RPG_BOSS_H
+#define DUNGEON_RPG_BOSS_H
+
+#include "StandardEnemy.h"
+
+class Player;
+
+class Boss : public StandardEnemy {
+private:
+    int maxHP = 100;
+    int phase = 1;
+
+public:
+    Boss();
+    Boss(const std::pair<int, int>& pos, const std::string& name, int enemyHealth,
+         int attackDamage, const std::pair<int, int>& enemySpeedMinAndMax, char symbolASCII);
+
+    void updatePhase();
+    void attack(Player& player);
+    [[nodiscard]] int getPhase() const;
+};
+
+#endif //DUNGEON_RPG_BOSS_H
